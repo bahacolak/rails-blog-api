6b60b008f0d8d65196075cb8fa673b0f9643b897
Bahadır Çolak <bahadircolak34@gmail.com>
2024-06-18 18:37:19 +0300

    password regex updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 8 }, format: { with: /\A(?=.*[A-Z])(?=.*\d)(?=.*[!@#$&*]).{8,}\z/, message: "must include at least one uppercase letter, one digit, and one special character" }, if: :password_required?
+  validates :password, presence: true, length: { minimum: 8 }, format: { with: /\A(?=.*[A-Z])(?=.*\d)(?=.*[!@#$&*()\-_=+\[\]{};:'",.<>?\\|`~^]).{8,}\z/, message: "must include at least one uppercase letter, one digit, and one special character" }, if: :password_required?
 end
