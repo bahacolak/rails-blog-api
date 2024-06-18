634a6ed1cafb80ac6b56ee20592a96a91a1c66b8
Bahadır Çolak <bahadircolak34@gmail.com>
2024-06-19 00:45:41 +0300

    register, login pages css files added

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
     port: 587,
     domain: 'gmail.com',
     user_name: 'bahadircolak34@gmail.com',
-    password: 'applicationpassword',
+    password: 'xxzxtehqhfpkqwto',
     authentication: 'plain',
     enable_starttls_auto: true
   }
